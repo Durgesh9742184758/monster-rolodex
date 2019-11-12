b6a9a6b372f434f6d4bc94b674682a4dde15e89f
Durgesh <nandandurgesh27@.com>
2019-11-12 21:28:50 +0530

    Updates

---
+++ precache-manifest.bbc732df13a743eb67d54fc67a704f7f.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1876e0d642c5f36aa0092019d157d4a8",
+    "url": "/monster-rolodex/index.html"
+  },
+  {
+    "revision": "da87f4946bc67f6515b3",
+    "url": "/monster-rolodex/static/css/main.8b2b7178.chunk.css"
+  },
+  {
+    "revision": "a5dd50631af588f6364b",
+    "url": "/monster-rolodex/static/js/2.62c8ec2f.chunk.js"
+  },
+  {
+    "revision": "da87f4946bc67f6515b3",
+    "url": "/monster-rolodex/static/js/main.1adb6146.chunk.js"
+  },
+  {
+    "revision": "897eb676deafa19a12a5",
+    "url": "/monster-rolodex/static/js/runtime-main.19508a17.js"
+  }
+]);
